4a96025dd7a44732e6d296b45bec0a89f43a11f0
Nelrak-John <rlguq616@gmail.com>
2026-07-08 15:53:56 +0900

    [기능] 스크롤 오류 수정

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,11 +8,13 @@ const Contact = () => {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        justifyContent: 'flex-start',
+        justifyContent: 'center',
         width: '100%',
         boxSizing: 'border-box',
-        padding: '300px 0px 150px 0px', 
-        minHeight: '70vh' 
+        padding: '0',
+        height: '100vh',
+        maxHeight: '100vh',
+        overflow: 'hidden'
       }}
     >
       {/* 1. 상단 Contact 메인 타이틀 */}
@@ -21,10 +23,7 @@ const Contact = () => {
         style={{
           width: '100%',
           textAlign: 'center',
-          
-          // 🚨 [가장 중요] 타이틀과 첫 번째 링크(KARLEN) 사이의 간격을 벌립니다.
-          // 현재 '-5px' 혹은 '0px'로 밀착되어 있다면, 이 값을 정방향 수치로 변경하십시오.
-          marginBottom: '-80px' // 수치를 키울수록 빨간 화살표처럼 아래로 밀려 내려갑니다.
+          marginBottom: '20px'
         }}
       >
       
